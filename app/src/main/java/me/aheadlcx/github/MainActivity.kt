445fa88df5d9fc7c26a2ae56007657de9beb524a
aheadlcx <ahead2008@gmail.com>
2022-11-26 18:49:52 +0800

    代码完善

--- app/src/main/java/me/aheadlcx/github/MainActivity.kt
+++ app/src/main/java/me/aheadlcx/github/MainActivity.kt
@@ -7,10 +7,9 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onCompletion
 import me.aheadlcx.github.api.GithubApiServiceManager
@@ -19,9 +18,7 @@ import me.aheadlcx.github.api.TokenBeanReq
 import me.aheadlcx.github.databinding.ActivityMainBinding
 import me.aheadlcx.util.rxLaunch
 
-class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
-
-    var scope = MainScope()
+class MainActivity : AppCompatActivity() {
 
     companion object {
         init {
@@ -37,9 +34,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     }
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mWebView: WebView
-
-
+    private var isLogin = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -51,7 +46,6 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     }
 
     private fun initWebView() {
-        binding.webView.settings.javaScriptEnabled = true
         binding.webView.settings.domStorageEnabled = true
         binding.webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         val webViewClient = MyWebViewClient(this)
@@ -61,18 +55,19 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     private fun initListener() {
         binding.txtUser.setOnClickListener(View.OnClickListener {
-//            getUserInfo()
-//            getUserInfoKotlin()
             getUserInfoFlow()
         })
         binding.txtLogin.setOnClickListener {
-            Log.i(TAG, "initListener: webView.loadUrl=${url}")
+            Log.i(TAG, "initListener: webView.loadUrl=${url},isLogin=${isLogin}")
+            if (isLogin) {
+                return@setOnClickListener
+            }
             binding.webView.loadUrl(url)
         }
     }
 
     private fun test() {
-        scope.rxLaunch<String> {
+        lifecycleScope.rxLaunch<String> {
             onRequest = {
                 "dataFromRequest"
             }
@@ -84,7 +79,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             }
         }
 
-        scope.launch {
+        lifecycleScope.launch {
             kotlin.runCatching {
                 "请求数据"
             }.onSuccess {
@@ -98,7 +93,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     }
 
     private fun getUserInfoFlow() {
-        launch {
+        lifecycleScope.launch {
             GithubApiServiceManager.gitHubService.getUserInfoByFlow(getOauth())
                 .flowOn(Dispatchers.IO)
                 .catch { cause ->
@@ -113,42 +108,15 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         }
     }
 
-    private fun getUserInfoKotlin() {
-        scope.launch {
-            val userInfo = RetrofitUtil.getGithubServiceKotlin(
-                RetrofitUtil.baseUrl_event
-            )
-                .getUserInfoSub(getOauth())
-            Log.i(TAG, "getUserInfoKotlin:userName= ${userInfo.login}")
-            binding.txtShowInfo.post({
-                binding.txtShowInfo.text = userInfo.login
-            })
-        }
-    }
-
     private fun getOauth(): String {
         return "Bearer " + RetrofitUtil.accessToken
     }
 
-    private fun getUserInfo() {
-        Log.i(TAG, "getUserInfo: ")
-        GlobalScope.launch(Dispatchers.IO) {
-//            val userInfo = RetrofitUtil.getGithubService().getUserInfo("Bearer " + RetrofitUtil.accessToken)
-            val userInfo = RetrofitUtil.getGithubService(
-                RetrofitUtil.baseUrl_event
-            )
-                .getUserInfo(getOauth())
-            try {
-                val response = userInfo.execute()
-                val body = response.body()
-                Log.i(TAG, "getUserInfo:isSuccessful=" + response.isSuccessful)
-                binding.txtShowInfo.post({
-                    binding.txtShowInfo.text = body?.login
-                })
-            } catch (e: Exception) {
-                Log.i(TAG, "getUserInfo: error=${e.message}")
-            }
-        }
+    private fun onHandleGetCode(code: String) {
+        binding.webView.visibility = View.GONE
+        getTokenByCode(code)
+        binding.txtLogin.text = "已经登录"
+        isLogin = true
     }
 
     private fun getTokenByCode(code: String) {
@@ -159,7 +127,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             it.client_id = client_id
             it.client_secret = client_secret
         }
-        launch {
+        lifecycleScope.launch {
             GithubApiServiceManager.gitHubService.getTokenByCodeFlow(req)
                 .flowOn(Dispatchers.IO)
                 .catch { cause ->
@@ -170,21 +138,6 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                     Log.i(TAG, "getTokenByCodeFlow: accessToken=${accessToken}")
                 }
         }
-        launch(Dispatchers.IO) {
-            val tokenByCode = RetrofitUtil.getGithubService().getTokenByCode(req)
-            try {
-                val response = tokenByCode.execute()
-                val body = response.body()
-                val toJson = Gson().toJson(body)
-                val accessToken = body?.access_token
-                RetrofitUtil.setAccessToken(accessToken)
-                Log.i(TAG, "getTokenByCode: accessToken=${accessToken}")
-                Log.i(TAG, "getTokenByCode: toJson=${toJson}")
-            } catch (e: Exception) {
-                Log.i(TAG, "getTokenByCode: Exception=${e.message}")
-
-            }
-        }
     }
 
     class MyWebViewClient(val activity: MainActivity) : WebViewClient() {
@@ -194,10 +147,14 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             Log.i(TAG, "shouldOverrideUrlLoading: redirect_uri=$redirect_uri")
             if (url.startsWith(redirect_uri) && url.startsWith(redirect_uri + "?code=")) {
                 val code: String = url.replace(redirect_uri + "?code=", "")
-                activity.getTokenByCode(code)
+                activity.onHandleGetCode(code)
             }
             return true
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     /**
